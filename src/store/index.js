7ebbeb6e4153abb8777fb513b4a7ba134e85b3b2
Dobbymin <gmin0701@knu.ac.kr>
2023-12-12 19:14:42 +0900

    ✨ feat: 코드 분할하기

--- src/store/index.js
+++ src/store/index.js
@@ -1,61 +1,10 @@
-import { createStore } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 
-export const INCREMENT = 'increment';
+import counterReducer from './counterSlice';
+import authReducer from './authSlice';
 
-const initialState = { counter: 0, showCounter: true };
-
-const counterReducer = (state = initialState, action) => {
-    /* 1 증가 */
-    if (action.type === INCREMENT) {
-        return {
-            counter: state.counter + 1,
-            showCounter: state.showCounter,
-        };
-    }
-
-    /* n 증가 */
-    if (action.type === 'increase') {
-        return {
-            counter: state.counter + action.value,
-            showCounter: state.showCounter,
-        };
-    }
-    /* 1 감소 */
-    if (action.type === 'decrement') {
-        return {
-            counter: state.counter - 1,
-            showCounter: state.showCounter,
-        };
-    }
-
-    /* 리셋 */
-    if (action.type === 'reset') {
-        return {
-            counter: (state.counter = 0),
-            showCounter: state.showCounter,
-        };
-    }
-
-    if (action.type === 'toggle') {
-        return {
-            showCounter: !state.showCounter,
-            counter: state.counter,
-        };
-    }
-
-    return state;
-};
-
-const store = createStore(counterReducer);
-
-const counterSubscriber = () => {
-    const latestState = store.getState();
-    console.log(latestState);
-};
-
-store.subscribe(counterSubscriber);
-
-store.dispatch({ type: 'increment' });
-store.dispatch({ type: 'decrement' });
+const store = configureStore({
+    reducer: { counter: counterReducer, auth: authReducer },
+});
 
 export default store;
